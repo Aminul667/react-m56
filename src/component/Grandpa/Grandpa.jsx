22b4eb35608a9e1c0e20c692ef3cbadf9a4e6d04
Aminul667 <rahathssn@gmail.com>
2023-04-08 22:24:47 +0600

    56-6 Simple Introduction to Context API to avoid Prop drilling

--- src/component/Grandpa/Grandpa.jsx
+++ src/component/Grandpa/Grandpa.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 import './Grandpa.css';
 
+export const RingContext = createContext('Gold');
+export const MoneyContext = createContext(0);
+
 const Grandpa = () => {
     const ring = 'Diamond';
+    const [money, setMoney] = useState(0);
+
     return (
         <div className='grandpa'>
             <h1>Hello Grandpa</h1>
-            <section className='flex'>
-                <Father ring={ring}></Father>
-                <Uncle></Uncle>
-                <Aunty ring={ring}></Aunty>
-            </section>
+            <p>Has money {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <RingContext.Provider value='Golden Ring'>
+                    <section className='flex'>
+                        <Father ring={ring}></Father>
+                        <Uncle></Uncle>
+                        <Aunty ring={ring}></Aunty>
+                    </section>
+                </RingContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
--- src/component/Special/Special.jsx
+++ src/component/Special/Special.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const Special = ({ring}) => {
+    const contextRing = useContext(RingContext);
     return (
         <div>
             <h2>Special</h2>
-            <p><small>{ring}</small></p>
+            <p><small>{contextRing}</small></p>
         </div>
     );
 };
